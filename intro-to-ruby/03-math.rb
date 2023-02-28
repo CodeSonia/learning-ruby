d57e26c190c9df57b43c9a1c87cfdd632f7690a1
sonia hussain <sonia.hussain@digital.cabinet-office.gov.uk>
2023-02-28 10:34:07 +0000

    Amended file names nams and learning how to use math in ruby

---
+++ intro-to-ruby/03-math.rb
@@ -0,0 +1,19 @@
+# Ruby isn't limited to assignments like mynum = 100, we can also do math!
+
+# addition +
+puts 10 + 10 # 20
+
+# subtraction -
+puts 10 - 5 # 5
+
+# Multiplication *
+puts 10 * 2 # 20
+
+# Division /
+puts 10 / 2 # 5
+
+# Exponentiation 
+puts 10 ** 4 # 10000
+
+# Modulo
+puts 10 % 2 # 0
