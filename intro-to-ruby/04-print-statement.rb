5776eccc0027520d8181b2ccaad513f29fe459a9
sonia hussain <sonia.hussain@digital.cabinet-office.gov.uk>
2023-02-28 10:38:35 +0000

    Difference between puts and print

---
+++ intro-to-ruby/04-print-statement.rb
@@ -0,0 +1,14 @@
+# The print commands takes whatever you give it and prints it to the console
+
+# "puts" for "put string" is slightly different
+# puts adds a new blank line after the thing you want it to print 
+
+# Notice you do not need any parentheses or semi-colons!
+
+puts "Hello"
+print "Sonia"
+print "Test!"
+
+# Output:
+# Hello
+# SoniaTest!
