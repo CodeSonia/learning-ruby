6bd1ad1934e6f4fa27dba84f94ba80e0c07d2e4f
sonia hussain <sonia.hussain@digital.cabinet-office.gov.uk>
2023-02-28 10:01:57 +0000

    Exploring diff data types in Ruby

---
+++ intro-to-ruby/data-types.rb
@@ -0,0 +1,14 @@
+# Numers, Strings or Booleans
+
+# There are 3 data types in Ruby: Numeric, Boolean, String
+
+my_num = 25
+my_boolean = true  
+my_string = "Ruby"  
+
+puts my_num
+# returns 25
+puts my_boolean
+# returns true 
+puts my_string
+# returns Ruby
