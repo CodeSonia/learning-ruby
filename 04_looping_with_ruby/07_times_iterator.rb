9d3d1ed42a12787c76893e13746517046f4cbb0f
sonia hussain <sonia.hussain@digital.cabinet-office.gov.uk>
2023-03-01 20:05:49 +0000

    Removed code example on file

--- 04_looping_with_ruby/07_times_iterator.rb
+++ 04_looping_with_ruby/07_times_iterator.rb
@@ -17,18 +17,3 @@ we might type:
 end
 
 # Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!Ruby!
-
-# puts "Text to search through: "
-# text = gets.chomp
-# puts "Word to redact: "
-# redact = gets.chomp
-
-# words = text.split(" ")
-
-# words.each do |word|
-#   if word != redact
-#     print word + " "
-#   else
-#     print "REDACTED "
-#   end
-# end
